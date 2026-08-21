0ad862771664c663e1f72fa01b13bae0b187edbb
nmizzy42-ux <nmizzy42@gmail.com>
2026-08-21 22:17:46 +1000

    User auth - delete as new version coming

--- 5.3D/controllers/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    bookController: require('./bookController')
-};
--- 5.3D/services/bookService.js
+++
@@ -1,9 +0,0 @@
-const Book = require('../models/bookModel');
-
-exports.getAllBooks = async () => {
-    return await Book.find({});
-};
-
-exports.getBookById = async (id) => {
-    return await Book.findOne({ id: id });
-};
